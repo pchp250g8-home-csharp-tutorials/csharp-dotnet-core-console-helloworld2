774bb2f2c754bd37738b0b65f9d6fb81703ce4b0
pchp250g8-home <pchp250g8@tutamail.com>
2025-09-28 18:28:31 +0300

    Добавьте файлы проекта.

---
+++ HelloWorld2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter your name:");
+var strName = Console.ReadLine();
+Console.WriteLine("Hello, " + strName + " !!!");
